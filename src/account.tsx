0d82702f5fe5d4c03925b1844e6b66c030ed815d
shaloo <shaloo@newfang.io>
2025-03-28 13:05:37 +0530

    Upgrade to v0.0.6 ca-wagmi' git push

--- src/account.tsx
+++ src/account.tsx
@@ -6,10 +6,10 @@ import {
   // useSendTransaction
 } from "wagmi";
 import {
-  useBalance,
   useSendTransaction,
   useWriteContract,
-  useUnifiedBalance,
+  useBalanceModal,
+  useBalance,
 } from "@arcana/ca-wagmi";
 import { Toast, Toaster, createToaster } from "@ark-ui/react/toast";
 
@@ -46,11 +46,8 @@ export function Account() {
   const { address } = useAccount();
   const { disconnect } = useDisconnect();
   const { data: ensName } = useEnsName({ address });
-  const { showBalance } = useBalance();
-  const { loading, getAssetBalance } = useUnifiedBalance();
-  if (!loading) {
-    console.log({ assetBalance: getAssetBalance("ETH") });
-  }
+  const { showModal } = useBalanceModal();
+  const { loading } = useBalance({ symbol: "ETH" });
   const { switchChainAsync } = useSwitchChain();
   const { writeContract } = useWriteContract();
 
@@ -223,7 +220,7 @@ export function Account() {
             </button>
             <button
               className="cursor-pointer text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
-              onClick={() => showBalance()}
+              onClick={() => showModal()}
             >
               Show balances
             </button>
